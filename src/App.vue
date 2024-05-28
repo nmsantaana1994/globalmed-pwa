1fa03599b8e13b49db74350b54f9cef771dfd1f9
nmsantaana1994 <nmsantaana1994@gmail.com>
2024-05-27 23:41:46 -0300

    Configurando App Vue

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,46 @@ export default {
 </script>
 
 <template>
-    <h1>Hola Mundo</h1>
+    <div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button
+                data-mdb-collapse-init
+                class="navbar-toggler"
+                type="button"
+                data-mdb-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                >
+                <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link disabled"
+                        >Disabled</a
+                    >
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main>
+            
+        </main>
+    </div>
 </template>
 
 <style scoped>
